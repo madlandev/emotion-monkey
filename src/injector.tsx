1ade898f4d8384fed43d859f8f048dcf76f1c0af
m1kak <mikayilg@madlan.co.il>
2018-08-14 16:07:36 +0300

    fix: package, build, typings

--- src/injector.tsx
+++ src/injector.tsx
@@ -1,8 +1,11 @@
 import { Emotion } from 'create-emotion';
 import * as React from 'react';
 import { Consumer } from './context';
-import { Subtract } from 'utility-types';
 
+
+type SetDifference<A, B> = A extends B ? never : A;
+type SetComplement<A, A1 extends A> = SetDifference<A, A1>;
+type Subtract<T extends T1, T1 extends object> = Pick<T, SetComplement<keyof T, keyof T1>>;
 
 export type Stylesheet = (emotion: Emotion) => any;
 
@@ -11,7 +14,7 @@ export interface InjectedProps {
 }
 
 export function injectEmotion(stylesheet: Stylesheet) {
-  return <P extends InjectedProps> (Comp: React.ComponentType<P>): React.ComponentType<Subtract<P, InjectedProps>> => (props) => (
+  return <P extends InjectedProps>(Comp: React.ComponentType<P>): React.ComponentType<Subtract<P, InjectedProps>> => (props) => (
     <Consumer>
       {(emotion) => {
         if (emotion === null) throw new Error('You likely forgot to wrap your app in EmotionProvider');
